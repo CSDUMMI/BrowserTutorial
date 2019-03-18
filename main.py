2efc63580bcd2e18bff1fbda785049f7eace6fb5
rexim <reximkut@gmail.com>
2019-03-18 21:26:37 +0100

    Code Editor with Syntax Highlighting

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 #! /usr/bin/python3
 from flask import Flask, render_template
 import json
+import markdown
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    tutorials_list = json.load(open("tutorials_list.json"))
-    return render_template("index.html",tutorials = tutorials_list)
-
-@app.route('/<tutorial>')
-def index_tutorial(tutorial_name):
-    """
-    <tutorial>.json:
-    {
-        "title": Title of the Tutorial must be compatible with Links,
+"""
+tutorials.json:
+{
+    <tutorial> { <tutorial> : Must be compatible with links, no withespaces
+        "title": Title of the Tutorial,
+        "description": Description of the Tutorial
         "lessons": [
             {
                 "title":Lesson Title,
@@ -24,18 +20,64 @@ def index_tutorial(tutorial_name):
             }
         ]
     }
-    """
-    tutorial = json.load(open(tutorial_name + ".json"))
-    return render_template("index_tutorial.html",
-                            title=tutorial_name,
-                            lessons=tutorial["lessons"]
+}
+"""
+
+@app.route("/")
+def index():
+    tutorials = json.load(open("tutorials.json"))
+    tutorials = zip(tutorials.keys(),tutorials.values())
+    return render_template  (
+                            "index.html",
+                            tutorials = tutorials
                             )
 
-@app.route('/<tutorial>/<lesson>')
-def showText(tutorial_name,lesson_name):
-    text_md = json.load(open(tutorial_name + ".json"))
+@app.route("/<tutorial>")
+def index_tutorial(tutorial):
+    tutorial_info = json.load(open("tutorials.json"))[tutorial]
+    tutorial_info["lessons"] = zip  (
+                                    range   (
+                                            0,
+                                            len(tutorial_info["lessons"])
+                                            ),
+                                            tutorial_info['lessons']
+                                    )
+    return render_template  (
+                            "index_tutorial.html",
+                            tutorial=tutorial,
+                            lessons=tutorial_info["lessons"]
+                            )
 
-    return render_template("showText.html", text=text_html)
+@app.route("/<tutorial>/<lesson_index>/text")
+def showText(tutorial,lesson_index):
+    lesson = json.load (open("tutorials.json"))
+    lesson = lesson[tutorial]
+    lesson = lesson["lessons"]
+    lesson = lesson[int(lesson_index)]
+
+    text_html   = markdown.markdown(str(lesson["text"]))
+    return render_template  (
+                            "text.html",
+                            text=text_html,
+                            tutorial=tutorial,
+                            lesson=lesson["title"]
+                            )
+
+@app.route("/<tutorial>/<lesson_index>/exercise")
+def showExercise(tutorial,lesson_index):
+    lesson = json.load (open("tutorials.json"))
+    lesson = lesson[tutorial]
+    lesson = lesson["lessons"]
+    lesson = lesson[int(lesson_index)]
+
+    code_template = lesson["code"]
+    return render_template  (
+                            "exercise.html",
+                            code=code_template
+                            )
+
+@app.route("/<tutorial>/<lesson_index>/")
+def showLesson(tutorial,lesson_index):
 
 if __name__ == '__main__':
     app.run()
