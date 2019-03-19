b501166a4b01f657e9bbfa15de2f6a10e980f20e
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-03-19 21:09:56 +0100

    Simple structure for the Test

--- tests/SampleTutorial/Lesson_0.py
+++ tests/SampleTutorial/Lesson_0.py
@@ -1,4 +1,14 @@
 import unittest, Lessons
 
+class TestLesson0(unittest.TestCase):
 
-lesson_0 = Lessons.Lesson(mode = "code",)
+    def test_concateStrings(self):
+        
+
+
+lesson_0_suite = unittest.TestLoader().loadTestsFromTestCase(TestLesson0)
+
+lesson_0 = Lessons.Lesson   (
+                            mode = "code",
+                            suite=lesson_0_suite
+                            )
