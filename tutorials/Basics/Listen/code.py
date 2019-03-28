a7f5c93bcc907d3be7384e45d6d546409c550267
aiscratch <aiscratcher@gmx.de>
2019-03-28 12:03:42 +0100

    Listen Lesson test and exercise

--- tutorials/Basics/Listen/code.py
+++ tutorials/Basics/Listen/code.py
@@ -1,2 +1,2 @@
-eine_ganze_Zahl =
-eine_dezimal_Zahl =
+head = 
+tail =
--- tutorials/Basics/Listen/test.py
+++ tutorials/Basics/Listen/test.py
@@ -1,12 +1,11 @@
 test_mode = "code"
 
 def test(local_vars):
-    if not (local_vars.get("eine_dezimal_Zahl") or local_vars.get("eine_ganze_Zahl")):
-        return "Fehler: eine_dezimal_Zahl oder eine_ganze_Zahl ist nicht definiert"
+    if not "head" in local_vars or not "tail" in local_vars:
+	return "Failure: head or tail isn't defined"
+    elif local_vars["head"] != local_vars["splitMe"][0]:
+	return "Failure: head isn't the head of splitMe"
+    elif local_vars["tail"] != local_vars["splitMe"][0:]:
+	return "Failure: tail isn't the tail of splitMe"
     else:
-        if local_vars["eine_dezimal_Zahl"] != 0.5:
-            return "Fehler: eine_dezimal_Zahl ist nicht 0.5"
-        elif local_vars["eine_ganze_Zahl"] != 50:
-            return "Fehler: eine_ganze_Zahl ist nicht 50"
-        else:
-            return "Success"
+	return "Success"
