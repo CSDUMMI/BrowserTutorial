2a0403d9c774d594d224410b9b75c33459bddeb2
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-03-28 12:28:14 +0100

    Correct test.py

--- tutorials/Basics/Listen/test.py
+++ tutorials/Basics/Listen/test.py
@@ -2,10 +2,10 @@ test_mode = "code"
 
 def test(local_vars):
     if not "head" in local_vars or not "tail" in local_vars:
-	return "Failure: head or tail isn't defined"
+        return "Failure: head or tail isn't defined"
     elif local_vars["head"] != local_vars["splitMe"][0]:
-	return "Failure: head isn't the head of splitMe"
+        return "Failure: head isn't the head of splitMe"
     elif local_vars["tail"] != local_vars["splitMe"][0:]:
-	return "Failure: tail isn't the tail of splitMe"
+        return "Failure: tail isn't the tail of splitMe"
     else:
-	return "Success"
+        return "Success"
